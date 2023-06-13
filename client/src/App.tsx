1ae144462f2aa15fc851756f9f59299c0c23a33b
Ameer Sabri <ameer.sabri@hotmail.co.uk>
2023-06-13 16:08:27 +0100

    feat: add normalize css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React from 'react';
+import 'normalize.css';
 
 function App() {
-  return (
-    <h1 className="text-center text-2xl font-medium text-red-700">
-      Hello World
-    </h1>
-  );
+	return <h1 className="text-center text-2xl font-medium text-red-700">Hello World</h1>;
 }
 
 export default App;
